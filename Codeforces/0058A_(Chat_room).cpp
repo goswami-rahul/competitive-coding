ee807d13607fb42f4b15d3ea9b8d03446f8c77f0
Rahul Goswami <irg8979@gmail.com>
2018-07-28 01:49:51 +0530

    28 July 18

---
+++ Codeforces/0058A_(Chat_room).cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector< int >
+#define vl vector< ll >
+#define pii pair< int, int >
+#define pll pair< ll , ll >
+#define pdd pair< double, double >
+#define vii vector< pii >
+#define vll vector< pll >
+#define vd vector< double >
+#define vb vector< bool >
+#define el "\n"
+#define fio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define ull unsigned long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define FOR(i, a, b) for(i=a; i<=b; i++)
+#define RFOR(i, b, a) for(i=b; i>=a; i--)
+#define all(vec) vec.begin(),vec.end()
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << "\n";
+	err(++it, args...);
+}
+
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+const double E=1e-6;
+
+/*************************************************************/
+
+int main()
+{
+    fio;
+    ll t=1, T;
+    ll i, j, x, y, k, w, a, b, n, m, d;
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+	string s;
+	i = 0;
+	cin >> s;
+	for (char c : s) {
+		switch(i) {
+			case 0:
+			if (c=='h') i++;
+			break;
+			case 1:
+			if (c=='e') i++;
+			break;
+			case 2:
+			if (c=='l') i++;
+			break;
+			case 3:
+			if (c=='l') i++;
+			break;
+			case 4:
+			if (c=='o') i++;
+			break;
+		}
+	}
+	if (i < 5) cout << "NO";
+	else cout << "YES";
+    return 0;
+}
