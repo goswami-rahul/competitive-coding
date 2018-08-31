a14bc9a61783a35f22a7619a72213283f6203eb7
Rahul Goswami <irg8979@gmail.com>
2018-09-01 02:57:30 +0530

    a2oj ladder

---
+++ Codeforces/0231C_(To_Add_or_Not_to_Add).cpp
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector< int >
+#define vl vector< ll >
+#define pii pair< int, int >
+#define pll pair< ll , ll >
+#define pdd pair< double, double >
+#define vii vector< pii >
+#define vll vector< pll >
+#define vd vector< double >
+#define vb vector< bool >
+#define el "\n"
+#define fio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define ull unsigned long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define FOR(i, a, b) for(ll i=a; i<=b; i++)
+#define RFOR(i, b, a) for(ll i=b; i>=a; i--)
+#define all(vec) (vec).begin(),(vec).end()
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " =: " << a << "\n";
+    // cerr << "\n";
+	err(++it, args...);
+}
+
+template <typename T1, typename T2>
+inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p)
+{
+    return os << "(" << p.first << ": " << p.second << ")";
+}
+
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v)
+{
+    bool first = true;
+    os << "[";
+    for(unsigned int i = 0; i < v.size(); i++)
+    {
+        if(!first)
+            os << ", ";
+        os << v[i];
+        first = false;
+    }
+    return os << "]";
+}
+
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::set<T>& v)
+{
+    bool first = true;
+    os << "{";
+    for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
+    {
+        if(!first)
+            os << ", ";
+        os << *ii;
+        first = false;
+    }
+    return os << "}";
+}
+
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::map<T1, T2>& v)
+{
+    bool first = true;
+    os << "{";
+    for (typename std::map<T1, T2>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
+    {
+        if(!first)
+            os << ", ";
+        os << *ii ;
+        first = false;
+    }
+    return os << "}";
+}
+
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_set<T>& v) {
+    return os << std::set<T>(v.begin(), v.end());
+}
+
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_map<T1, T2>& v) {
+    return os << std::map<T1, T2>(v.begin(), v.end());
+}
+
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+const double E=1e-6;
+
+/***********************************************************************/
+
+int main()
+{
+    fio;
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    ll n, x, k;
+    cin >> n >> k;
+    vector<ll> a(n + 1);
+    a[0] = -INF;
+    for (int i = 1; i < n + 1; ++i)
+        cin >> a[i];
+    sort(all(a));
+    vector<ll> pre(n + 1);
+    pre[0] = 0;
+    for (int i = 1; i <= n; ++i)
+        pre[i] = pre[i - 1] + a[i];
+    ll bestmax = -1, bestx;
+    for (int i = 1; i <= n; ++i) {
+        int lo = 1, hi = i, mid;
+        while (lo < hi) {
+            mid = (lo + hi) / 2;
+            ll cost = (a[i] * (i - mid + 1)) - (pre[i] - pre[mid - 1]);
+            if (cost <= k)
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+        int cur = i - hi + 1;
+        if (cur > bestmax) {
+            bestmax = cur;
+            bestx = a[i];
+        }
+    }
+    cout << bestmax << " " << bestx;
+
+    return 0;
+}
