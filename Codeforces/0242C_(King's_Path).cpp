0912ef1d9195bdbbf2d2c87c60a48ff13ed18b98
Rahul Goswami <irg8979@gmail.com>
2018-09-14 00:47:40 +0530

    a2oj ladder

---
+++ Codeforces/0242C_(King's_Path).cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector< int >
+#define vl vector< ll >
+#define pii pair< int, int >
+#define pll pair< ll , ll >
+#define pdd pair< double, double >
+#define vii vector< pii >
+#define vll vector< pll >
+#define vd vector< double >
+#define vb vector< bool >
+#define el "\n"
+#define ull unsigned long long int
+#define ld long double
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define FOR(i, a, b) for(ll i=a; i<=b; i++)
+#define RFOR(i, b, a) for(ll i=b; i>=a; i--)
+#define all(vec) (vec).begin(),(vec).end()
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " =: " << a << "\n";
+    // cerr << "\n";
+	err(++it, args...);
+}
+
+template <typename T1, typename T2>
+inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p)
+{
+    return os << "(" << p.first << ": " << p.second << ")";
+}
+
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v)
+{
+    bool first = true;
+    os << "[";
+    for(unsigned int i = 0; i < v.size(); i++)
+    {
+        if(!first)
+            os << ", ";
+        os << v[i];
+        first = false;
+    }
+    return os << "]";
+}
+
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::set<T>& v)
+{
+    bool first = true;
+    os << "{";
+    for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
+    {
+        if(!first)
+            os << ", ";
+        os << *ii;
+        first = false;
+    }
+    return os << "}";
+}
+
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::map<T1, T2>& v)
+{
+    bool first = true;
+    os << "{";
+    for (typename std::map<T1, T2>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
+    {
+        if(!first)
+            os << ", ";
+        os << *ii ;
+        first = false;
+    }
+    return os << "}";
+}
+
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_set<T>& v) {
+    return os << std::set<T>(v.begin(), v.end());
+}
+
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_map<T1, T2>& v) {
+    return os << std::map<T1, T2>(v.begin(), v.end());
+}
+
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+const double E = 1e-6;
+/***********************************************************************/
+
+int main()
+{
+    ll sx, sy, dx, dy;
+    cin >> sx >> sy >> dx >> dy;
+    int n;
+    cin >> n;
+    map<pair<ll, ll>, ll> good;
+    ll x, y, r;
+    while (n--) {
+        cin >> r >> x >> y;
+        for (ll c = x; c <= y; ++c)
+            good[{r, c}] = -1;
+    }
+    queue<pair<ll, ll>> q;
+    q.push({sx, sy});
+    good[{sx, sy}] = 0LL;
+    while (!q.empty()) {
+        ll x1, y1, d;
+        d = good[q.front()];
+        tie(x1, y1) = q.front(); q.pop();
+        for (ll x = -1; x <= 1; ++x)
+            for (ll y = -1; y <= 1; ++y)
+                if (good.count({x1 + x, y1 + y}) && good[{x1 + x, y1 + y}] == -1) {
+                    q.push({x1 + x, y1 + y});
+                    good[{x1 + x, y1 + y}] = d + 1;
+                }
+    }
+    cout << good[{dx, dy}];
+
+    return 0;
+}
