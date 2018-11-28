7327eb129fdd817e39926b9bcd198fdec800a220
Rahul Goswami <irg8979@gmail.com>
2018-11-29 01:49:05 +0530

    contest 28 Nov 2018

---
+++ Codeforces/1082A_(Vasya_and_Book).cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+template<typename T>
+using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+using ll = long long int;
+using i64 = long long int;
+#define all(vec) (vec).begin(),(vec).end()
+template <typename T> inline void ckmax(T &x, T y) {if (y > x) x = y; }
+template <typename T> inline void ckmin(T &x, T y) {if (y < x) x = y; }
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " =: " << a << endl;
+	err(++it, args...);
+}
+template <typename T1, typename T2>
+inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& buf) {
+    return os << "(" << buf.first << ": " << buf.second << ")";
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v) {
+    bool first = true;
+    os << "[";
+    for(unsigned int i = 0; i < v.size(); i++) {
+        if(!first) os << ", ";
+        os << v[i];
+        first = false;
+    }
+    return os << "]";
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::set<T>& v) {
+    bool first = true;
+    os << "{";
+    for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
+        if(!first) os << ", ";
+        os << *ii;
+        first = false;
+    }
+    return os << "}";
+}
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::map<T1, T2>& v) {
+    bool first = true;
+    os << "{";
+    for (typename std::map<T1, T2>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
+        if(!first) os << ", ";
+        os << *ii ;
+        first = false;
+    }
+    return os << "}";
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_set<T>& v) {
+    return os << std::set<T>(v.begin(), v.end());
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::multiset<T,greater<T>>& v) {
+    return os << std::vector<T>(v.begin(), v.end());
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const ordered_set<T>& v) {
+    return os << std::set<T>(v.begin(), v.end());
+}
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::unordered_map<T1, T2>& v) {
+    return os << std::map<T1, T2>(v.begin(), v.end());
+}
+
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+const double EPS = 1e-6;
+/***********************************************************************/
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        i64 n, x, y, d;
+        cin >> n >> x >> y >> d;
+        i64 ans = INF;
+        --x; --y;
+        if (((i64) abs(y - x)) % d == 0) {
+            ans = min(ans, (i64) abs(y - x) / d);
+            cout << ans << '\n';
+            continue;
+        }
+        if (y % d == 0) {
+            ans = min(ans, y / d + (x + d - 1) / d);
+        }
+        if ((n - y - 1) % d == 0) {
+            ans = min(ans, (n - y - 1) / d + (n - x - 1 + d - 1) / d);
+        }
+        if (ans == INF) ans = -1;
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
