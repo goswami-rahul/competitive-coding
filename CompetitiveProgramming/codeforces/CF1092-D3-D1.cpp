d3289d94c6be33e0cab351bd2602702c402261ce
Rahul Goswami <irg8979@gmail.com>
2019-01-29 23:01:14 +0530

    update cf

---
+++ CompetitiveProgramming/codeforces/CF1092-D3-D1.cpp
@@ -0,0 +1,107 @@
+/*
+ * Store only the parities of the numbers, and
+ * then greedily delete even length same parity segments.
+ * output yes if only 1 segment remains at last.
+ */
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+template<typename T, typename V = __gnu_pbds::null_type>
+using ordered_set = __gnu_pbds::tree<T, V, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // find_by_order(), order_of_key()
+using ll = long long int;
+using i64 = long long int;
+#define SZ(v) (int((v).size()))
+#define ALL(vec) (vec).begin(),(vec).end()
+template <typename T> inline void ckmax(T &x, T y) {if (y > x) x = y; }
+template <typename T> inline void ckmin(T &x, T y) {if (y < x) x = y; }
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " =: " << a << endl;
+    err(++it, args...);
+}
+template <typename T1, typename T2>
+inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& buf) {
+    return os << "(" << buf.first << ": " << buf.second << ")";
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v) {
+    bool first = true;
+    os << "[";
+    for(unsigned int i = 0; i < v.size(); i++) {
+        if(!first) os << ", ";
+        os << v[i];
+        first = false;
+    }
+    return os << "]";
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const multiset<T>& v) {
+    bool first = true;
+    os << "{";
+    for (auto ii = v.begin(); ii != v.end(); ++ii) {
+        if(!first) os << ", ";
+        os << *ii;
+        first = false;
+    }
+    return os << "}";
+}
+inline std::ostream &operator << (std::ostream & os,const std::multiset<pair<int,int>,greater<pair<int,int>>>& v) {
+    bool first = true;
+    os << "{";
+    for (auto x: v) {
+        if(!first) os << ", ";
+        os << x;
+        first = false;
+    }
+    return os << "}";
+}
+template<typename T1, typename T2>
+inline std::ostream &operator << (std::ostream & os,const std::map<T1, T2>& v) {
+    bool first = true;
+    os << "{";
+    for (typename std::map<T1, T2>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
+        if(!first) os << ", ";
+        os << *ii ;
+        first = false;
+        return os << "}";
+    }
+}
+template<typename T>
+inline std::ostream &operator << (std::ostream & os,const vector<vector<T>>& v) {
+    for (auto &u: v) os << '\n' << u;
+    return os;
+}
+template<typename T>
+void finish(T ans) {cout << ans << '\n'; exit(0);}
+const int MOD = 1e9 + 7;
+const long long INF = 1e18;
+const double EPS = 1e-6;
+#define PI 3.14159265358979323846
+/***********************************************************************/
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &e: a) cin >> e, e &= 1;
+    vector<int> s;
+    for (int i = 0; i < n; ++i) {
+        if (!s.empty() && s.back() == a[i]) {
+            s.pop_back();
+        } else {
+            s.push_back(a[i]);
+        }
+    }
+    cout << (SZ(s) < 2 ? "YES\n" : "NO\n");
+    
+    return 0;
+}
