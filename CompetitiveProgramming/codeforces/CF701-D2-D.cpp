48bb9217f4025af0f7fdfffc890943d2706a04d8
Rahul Goswami <irg8979@gmail.com>
2019-02-09 02:03:55 +0530

    update cf

--- CompetitiveProgramming/codeforces/CF701-D2-D.cpp
+++ CompetitiveProgramming/codeforces/CF701-D2-D.cpp
@@ -1,6 +1,10 @@
 /*
- * The greedy solution is to slide a window of length l, and
- * the answer is the minimum sum of such window.
+ * We binary search on the position x such that, if we drop
+ * the first passenger on bus at x, then he and the last
+ * passenger reach at the destination at the exact same time.
+ * If we find such x, it is optimal to always drop the first
+ * person at x, and the answer will be the time for x distance
+ * using bus, and the rest on foot.
  */
 #include<bits/stdc++.h>
 using namespace std;
@@ -82,25 +86,33 @@ const long long INF = 1e18;
 const double EPS = 1e-6;
 #define PI 3.14159265358979323846
 /***********************************************************************/
-
+inline bool f(long double x, int v1, int v2, int m, int d) {
+    long double p = 0;
+    for (int i = 0; i < m - 1; ++i) {
+        p += 2 * (x - p) * v1 / (v1 + v2);
+        x += (x - p) * v1 / v2;
+        x = (x * v2 - p * v1) / (v2 - v1);
+    }
+    return x >= d;
+}
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr); cout << fixed; cout.precision(10);
     
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n - 1);
-    for (int &e: a) cin >> e;
-
-    int ans = MOD, sum = 0;
-    for (int i = 0; i < n - 1; ++i) {
-        sum += a[i];
-        if (i - m >= 0) {
-            sum -= a[i - m];
+    int n, d, v1, v2, k;
+    cin >> n >> d >> v1 >> v2 >> k;
+    long double lo = 0, hi = d;
+    int m = (n + k - 1) / k;
+    for (int iter = 0; iter < 200; ++iter) {
+        long double mid = (lo + hi) / 2.0;
+        if (f(mid, v1, v2, m, d)) {
+            hi = mid;
+        } else {
+            lo = mid;
         }
-        if (i + 1 >= m) uin(ans, sum);
     }
+    long double ans = lo / v2 + (d - lo) / v1;
     cout << ans << '\n';
     
     return 0;
