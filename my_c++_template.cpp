07f8c14782a76d65b9360cdb721f8a89bf2bf035
Rahul Goswami <irg8979@gmail.com>
2019-04-05 01:35:31 +0530

    Update my_c++_template.cpp

--- my_c++_template.cpp
+++ my_c++_template.cpp
@@ -1,110 +1,87 @@
-#include<bits/stdc++.h>
+#pragma GCC optimize(3)
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-
-template<typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#define ll long long int
-#define vi vector< int >
-#define vl vector< ll >
-#define pii pair< int, int >
-#define pll pair< ll , ll >
-#define pdd pair< double, double >
-#define vii vector< pii >
-#define vll vector< pll >
-#define vd vector< double >
-#define vb vector< bool >
-#define el "\n"
-#define ull unsigned long long int
-#define ld long double
-#define fi first
-#define se second
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define sqr(x) ((x) * (x))
-#define FOR(i, a, b) for(ll i=a; i<=b; i++)
-#define RFOR(i, b, a) for(ll i=b; i>=a; i--)
-#define all(vec) (vec).begin(),(vec).end()
-
+#define SZ(v) (int((v).size()))
+#define ALL(vec) (vec).begin(),(vec).end()
 #define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-void err(istream_iterator<string> it) {}
+using namespace std;
+template<typename T, typename V = __gnu_pbds::null_type>
+using ordered_set = __gnu_pbds::tree<T, V, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // find_by_order(), order_of_key()
+ 
+using ll = long long int;
+using i64 = long long int;
+template <typename T> inline bool uax(T &x, T y) {return (y > x) ? x = y, true : false;}
+template <typename T> inline bool uin(T &x, T y) {return (y < x) ? x = y, true : false;}
+ 
+void err(__attribute__((unused)) istream_iterator<string> it) {}
 template<typename T, typename... Args>
 void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " =: " << a << "\n";
-    // cerr << "\n";
-	err(++it, args...);
-}
-template<class T1, class T2, class T3>
-struct triple {
-    T1 a; T2 b; T3 c;
-    triple() : a(T1()), b(T2()), c(T3()) {};
-    triple(T1 _a, T2 _b, T3 _c) :a(_a), b(_b), c(_c) {}
-};
-template<class T1, class T2, class T3>
-bool operator<(const triple<T1,T2,T3>&t1,const triple<T1,T2,T3>&t2) {
-    if(t1.a!=t2.a)return t1.a<t2.a;
-    else if(t1.b!=t2.b)return t1.b<t2.b;
-    else return t1.c<t2.c;
+  cerr << *it << " =: " << a << endl;
+  err(++it, args...);
 }
 template <typename T1, typename T2>
-inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p) {
-    return os << "(" << p.first << ": " << p.second << ")";
+inline ostream& operator << (ostream& os, const pair<T1, T2>& buf) {
+  return os << "(" << buf.first << ": " << buf.second << ")";
 }
 template<typename T>
-inline std::ostream &operator << (std::ostream & os,const std::vector<T>& v) {
-    bool first = true;
-    os << "[";
-    for(unsigned int i = 0; i < v.size(); i++) {
-        if(!first) os << ", ";
-        os << v[i];
-        first = false;
-    }
-    return os << "]";
+inline ostream &operator << (ostream & os,const vector<T>& v) {
+  bool first = true;
+  os << "[";
+  for (const auto &e: v) {
+    if(!first) os << ", ";
+    os << e;
+    first = false;
+  }
+  return os << "]";
 }
 template<typename T>
-inline std::ostream &operator << (std::ostream & os,const std::set<T>& v) {
-    bool first = true;
-    os << "{";
-    for (typename std::set<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
-        if(!first) os << ", ";
-        os << *ii;
-        first = false;
-    }
-    return os << "}";
+inline ostream &operator << (ostream & os, multiset<T>& v) {
+  bool first = true;
+  os << "{";
+  for (auto ii = v.begin(); ii != v.end(); ++ii) {
+    if(!first) os << ", ";
+    os << *ii;
+    first = false;
+  }
+  return os << "}";
+}
+template<typename T>
+inline ostream &operator << (ostream & os,const unordered_set<T>& v) {
+  bool first = true;
+  os << "{";
+  for (const auto &x : v) {
+    if(!first) os << ", ";
+    os << x;
+    first = false;
+  }
+  return os << "}";
 }
 template<typename T1, typename T2>
-inline std::ostream &operator << (std::ostream & os,const std::map<T1, T2>& v) {
-    bool first = true;
-    os << "{";
-    for (typename std::map<T1, T2>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
-        if(!first) os << ", ";
-        os << *ii ;
-        first = false;
-    }
-    return os << "}";
+inline ostream &operator << (ostream & os,const map<T1, T2>& v) {
+  bool first = true;
+  os << "{";
+  for (const auto &e : v) {
+    if(!first) os << ", ";
+    os << e;
+    first = false;
+  }
+  return os << "}";
 }
 template<typename T>
-inline std::ostream &operator << (std::ostream & os,const std::unordered_set<T>& v) {
-    return os << std::set<T>(v.begin(), v.end());
+ostream &operator << (ostream & os,const vector<vector<T>>& v) {
+  for (auto &u: v) os << '\n' << u;
+  return os;
 }
-template<typename T1, typename T2>
-inline std::ostream &operator << (std::ostream & os,const std::unordered_map<T1, T2>& v) {
-    return os << std::map<T1, T2>(v.begin(), v.end());
-}
-
-const ll MOD = 1e9+9;
-const ll INF = 1e18;
-const double EPS = 1e-6;
+template<typename T>
+void finish(T ans) {cout << ans << '\n'; exit(0);}
+int const MOD = 1e9 + 7;
+long long const INF = 1e18;
+#define PI 3.14159265358979323846
 /***********************************************************************/
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    return 0;
+signed main() {
+  
+  return 0;
 }
-
