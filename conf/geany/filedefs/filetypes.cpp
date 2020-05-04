6216103ff0258797c54365ae0300b2799ad1a472
Rahul Goswami <irg8979@gmail.com>
2020-05-05 02:57:58 +0530

    update geany snippets

--- conf/geany/filedefs/filetypes.cpp
+++ conf/geany/filedefs/filetypes.cpp
@@ -1,6 +1,6 @@
 [build-menu]
 FT_00_LB=_Compile
-FT_00_CM=g++ -std=c++11  "%f"
+FT_00_CM=g++ -std=c++14 -O2  "%f"  -DRahul 
 FT_00_WD=
 FT_01_LB=_Build
 FT_01_CM=g++ -DRahul  -Wall -Wextra  -std=c++17  -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-misleading-indentation -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector "%f"
