2e69b6d1dbe25bab6fa9d4043993dd02e43d583e
Rahul Goswami <irg8979@gmail.com>
2020-05-24 00:16:19 +0530

    create RAHUL.h

---
+++ RAHUL.h
@@ -0,0 +1,24 @@
+/**
+ * Place at -- /usr/local/include/ 
+ * Precompile --
+g++ -H -Wall -Wextra -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-misleading-indentation -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector RAHUL.h
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); \
+  stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+string to_string(char c) { return "'" + string(1, c) + "'";}
+string to_string(string s) { return '"' + s + '"';}
+string to_string(const char* s) { return to_string((string) s);}
+template<typename A> string to_string(A);
+template<typename A, typename B> string to_string(pair<A, B> p){
+  return "(" + to_string(p.first) + ": " + to_string(p.second) + ")";}
+template<typename A> string to_string(A v) {bool f = false; string r = "{"; 
+  for (auto x: v) {if (f) r += ", "; r += to_string(x); f = true;} return r += "}";}
+template<typename A> string to_string(vector<vector<A>> v) {string r; 
+  for (auto x: v) r += "\n" + to_string(x); return r;}
+int Nerr;
+template<typename A> string to_string(A *p) {return to_string(vector<A>(p, p + Nerr));}
+void err(istream_iterator<string>) { cerr << endl; }
+template<typename T,typename... Args> void err(istream_iterator<string> it, T a, Args... args) {
+  cerr << *it << " =: " << to_string(a) << "; "; err(++it, args...); }
