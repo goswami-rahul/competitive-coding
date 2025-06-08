2e71b3a0255a03d4d8642fc0054b1da4d8089807
Rahul Goswami <irg8979@gmail.com>
2025-06-09 03:05:00 +0530

    minor update to basis

--- library/Basis.cpp
+++ library/Basis.cpp
@@ -1,32 +1,48 @@
+
 template<typename T, int K>
 struct Basis {
   array<T, K> bas;
+  array<T, K> pos;
   int n, rank;
   Basis() : bas(), n(), rank() {}
-  bool add(T x) {
+  bool add(T x, int idx) {
+    T subset = T(1) << idx;
     ++n;
     for (int i = K - 1; i >= 0; --i) {
       if (x >> i & 1) {
         if (bas[i]) {
           x ^= bas[i];
+          subset ^= pos[i];
         } else {
           bas[i] = x, ++rank;
+          pos[i] = subset;
           return true;
         }
       }
     }
     return false;
   }
+  int can(T x) {
+    for (int i = K - 1; i >= 0; --i) {
+      if (x >> i & 1) x ^= bas[i];
+    }
+    return x == 0;
+  }
+  T xorset(T x) {
+    if (!can(x)) return -1;
+    T res = 0;
+    for (int i = K - 1; i >= 0; --i) {
+      if (x >> i & 1) {
+        x ^= bas[i];
+        res ^= pos[i];
+      }
+    }
+    return res;
+  }
   int ways(T x) {
     for (int i = K - 1; i >= 0; --i) {
       if (x >> i & 1) x ^= bas[i];
     }
     return x == 0 ? mpow(2, n - rank) : 0;
-  }
-  Basis operator + (Basis o) const {
-    int nn = n + o.n;
-    for (int i = 0; i < K; ++i) if (bas[i]) o.add(bas[i]);
-    o.n = nn;
-    return o;
   }
 };
