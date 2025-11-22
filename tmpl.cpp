2e68ea0f64657b0548eec513bfc3322385fa8bfb
Rahul Goswami <irg8979@gmail.com>
2025-11-23 04:29:24 +0530

    update stuff

--- tmpl.cpp
+++ tmpl.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 #define error(...) 42;
 #endif
-#define SZ(v) int((v).size())
-#define ALL(vec) begin(vec), end(vec)
 template<typename T> inline bool uax(T &x, T y) {return (y > x) ? x = y, true : false;}
 template<typename T> inline bool uin(T &x, T y) {return (y < x) ? x = y, true : false;}
-template<typename T> void kek(T ans) {cout << ans << endl; exit(0);}
 #define Luv(...) [&] (auto &&u, auto &&v) { return __VA_ARGS__; }
 using i64 = int64_t;
-const int MOD = (int) 1e9 + 7;
-const i64 INF = (i64) 1e18 + 42;
 
-int32_t main() {
-  cin.tie(nullptr) -> sync_with_stdio(false);
+void SolveCase() {
   
+}
+
+int main() {
+  cin.tie(nullptr) -> sync_with_stdio(false);
+  int tt; cin >> tt;
+  while (tt--) SolveCase();
 }
